17f86fa3cdad05c02a692c7ee6b619e47b423b09
Ariel Monroy <70923488+ArielMonroy99@users.noreply.github.com>
2025-03-21 23:55:38 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: "https://arimon.xyz",
+    origin: "https://www.arimon.xyz",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     credentials: true,
   })
